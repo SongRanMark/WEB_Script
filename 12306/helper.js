5282986bd5f3384827a105fed34cc0bdd448c3f8
Mark Song <marksong@augmentum.com.cn>
2016-12-15 18:59:09 +0800

    Support that can select a train to buy ticket.

--- 12306/helper.js
+++ 12306/helper.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         12306 Helper
 // @version      0.1
-// @description  Automatic operation
+// @description  12306 Helper
 // @author       S.R
+// @include      https://kyfw.12306.cn/otn/leftTicket/init
 // @require      file:///Web_Script/12306/helper.js
 // @resource     helperCSS file:///Web_Script/12306/helper.css
 // @grant        GM_addStyle
@@ -13,9 +14,24 @@
 var cssString = GM_getResourceText("helperCSS");
 GM_addStyle(cssString);
 
-var operationButtonHTML = "<button type='button' id='helper-query-button'>查询</button>";
+// 记录列表中用户选择将要抢票的列车索引，在请求到的最新数据中通过该索引获取对应列车数据，检查是否可以下单。
+var selectedTrainIndex = 0;
+var operationButtonHTML = "<button type='button' id='helper-query-button'>自动预定</button>";
 $("#t-list").prepend(operationButtonHTML);
-$("#helper-query-button").click(function () {
+$("#helper-query-button").click(function() {
+    if (!isThereTrainList()) {
+        alert("请先查询车次列表，并选择要自动预定的车次");
+        return;
+    };
+
+    canSelectedTrainTicketBuy(selectedTrainIndex);
+});
+
+function isThereTrainList() {
+    return $("#queryLeftTable").html() != "";
+}
+
+function canSelectedTrainTicketBuy(index) {
     var parameters = {
         "leftTicketDTO.train_date": $.trim($("#train_date").val()),
         "leftTicketDTO.from_station": $("#fromStation").val(),
@@ -33,16 +49,41 @@ $("#helper-query-button").click(function () {
         data: parameters,
         timeout: 10000,
         success: function(result, status, xhr) {
-            // var record = result.data[0];
+            var record = result.data[index];
 
-            // if ("Y" == record.queryLeftNewDTO.canWebBuy) {
-            //     checkG1234(record.secretStr, record.queryLeftNewDTO.start_time, record.queryLeftNewDTO.train_no, record.queryLeftNewDTO.from_station_telecode, record.queryLeftNewDTO.to_station_telecode);
-            // } else {
-            //     $("#helper-query-button").click();
-            // }
+            if ("Y" == record.queryLeftNewDTO.canWebBuy) {
+                buyTrainTicket(record);
+            } else {
+                canSelectedTrainTicketBuy(index);
+            }
         },
         error: function(xhr, status, error) {
-            // $("#helper-query-button").click();
+            canSelectedTrainTicketBuy(index);
         }
+    });
+}
+
+function buyTrainTicket(record) {
+    checkG1234(record.secretStr, record.queryLeftNewDTO.start_time, record.queryLeftNewDTO.train_no, record.queryLeftNewDTO.from_station_telecode, record.queryLeftNewDTO.to_station_telecode);
+}
+
+$("#t-list").bind('DOMNodeInserted', function(e) {
+    if ("queryLeftTable" != e.target.id) {
+        return;
+    }
+
+    var trainList = $("#queryLeftTable").children("[id^='ticket_']");
+    var index = 0;
+    trainList.each(function() {
+        var radioHTML = "<input type='radio' name='trainList' ";
+        radioHTML += index == 0 ? "checked = 'checked' " : "";
+        radioHTML += "value='"+ index +"'/>";
+        var otherItem = $(this).children("[id^='QT_']");
+        otherItem.html(radioHTML);
+        otherItem.removeAttr("onclick");
+        index++;
+    });
+    $("input[name='trainList']").change(function() {
+        selectedTrainIndex = Number($(this).val());
     });
 });
